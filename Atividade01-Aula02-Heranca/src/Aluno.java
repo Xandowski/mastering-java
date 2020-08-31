58b044170b887d4512980f3cecd3d13d7fa525da
Xandowski <xandy-moraes@hotmail.com>
2020-08-31 16:59:32 -0300

    primeiros conceitos sobre herança

---
+++ Atividade01-Aula02-Heranca/src/Aluno.java
@@ -0,0 +1,21 @@
+
+public class Aluno extends Pessoa{
+	private long ra;
+	private long enem;
+	
+	public void setRa(long ra) {
+		this.ra = ra;
+	}
+	
+	public void setSalario(long enem) {
+		this.enem = enem;
+	}
+	
+	public long getRa() {
+		return ra;
+	}
+	
+	public long getSalario() {
+		return enem;
+	}
+}
---
+++ Atividade01-Aula02-Heranca/src/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa {
+	private String nome;
+	private String email;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+}
