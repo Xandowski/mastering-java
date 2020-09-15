33d0399cec1b30fc7ca3bc9450ee634aeb07783e
Xandowski <xandy-moraes@hotmail.com>
2020-09-14 23:22:36 -0300

    atividade 3 da aula 4

---
+++ Atividade03-Aula04/src/Monografia.java
@@ -0,0 +1,47 @@
+public class Monografia {
+	private long numero;
+	private double titulo;
+	private Aluno aluno;
+	private Professor professor;
+	private Disciplina disciplina;
+	
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+	
+	public void setTitulo(double titulo) {
+		this.titulo = titulo;
+	}
+	
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+	
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+	
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	public long getNumero() {
+		return numero;
+	}
+	
+	public double getTitulo() {
+		return titulo;
+	}
+	
+	public Aluno getAluno() {
+		return aluno;
+	}
+	
+	public Professor getProfessor() {
+		return professor;
+	}
+	
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+}
