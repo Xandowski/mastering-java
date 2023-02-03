10660f4959293c6c0e9a52eaa6f754eff7e241e8
adocarmomor2 <a.docarmomor2@dxc.com>
2023-02-03 20:29:01 -0300

    Ordenação em Map

--- working-with-collections/src/Map/Book.java
+++ working-with-collections/src/Map/Book.java
@@ -1,6 +1,9 @@
 package Map;
 
-public class Book {
+import java.util.Comparator;
+import java.util.Map;
+
+public class Book implements Comparable<Book> {
   String author;
   String bookName;
   int pages;
@@ -35,4 +38,64 @@ public class Book {
     this.pages = pages;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((author == null) ? 0 : author.hashCode());
+    result = prime * result + ((bookName == null) ? 0 : bookName.hashCode());
+    result = prime * result + pages;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Book other = (Book) obj;
+    if (author == null) {
+      if (other.author != null)
+        return false;
+    } else if (!author.equals(other.author))
+      return false;
+    if (bookName == null) {
+      if (other.bookName != null)
+        return false;
+    } else if (!bookName.equals(other.bookName))
+      return false;
+    if (pages != other.pages)
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Book: {author=" + author + ", bookName=" + bookName + ", pages=" + pages + "}";
+  }
+
+  @Override
+  public int compareTo(Book book) {
+    int pages = Integer.compare(this.getPages(), book.getPages());
+
+    if (pages != 0)
+      return pages;
+
+    int author = this.getAuthor().compareTo(book.getAuthor());
+
+    if (author != 0)
+      return author;
+
+    return this.getBookName().compareTo(book.getBookName());
+  }
+}
+
+class BookNameComparator implements Comparator<Map.Entry<String, Book>> {
+  @Override
+  public int compare(Map.Entry<String, Book> b1, Map.Entry<String, Book> b2) {
+    return b1.getValue().getBookName().compareToIgnoreCase(b2.getValue().getBookName());
+  }
 }
--- working-with-collections/src/Map/Ordenation.java
+++ working-with-collections/src/Map/Ordenation.java
@@ -1,5 +1,59 @@
 package Map;
 
-public class Ordenation {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+public class Ordenation {
+  public static void main(String[] args) {
+    System.out.println("Ordem aleátoria.");
+    Map<String, Book> myBooks = new HashMap<>() {
+      {
+        put("Pushkin Press", new Book("Robert W. Chambers", "The king in yellow", 316));
+        put("Alma Books", new Book("F Scott Fitzgerald", "The Great Gatsby", 256));
+        put("Orbit", new Book("Andrzej Sapkowski", "The Last Wish: Introducing the Witcher", 384));
+      }
+    };
+
+    myBooks.forEach((key, value) -> {
+      System.out.println("Publishing : " + key + " - " + value.toString());
+    });
+
+    System.out.println("--  Ordem de inserção.  --");
+
+    Map<String, Book> myBooksLinkedMap = new LinkedHashMap<>() {
+      {
+        put("Pushkin Press", new Book("Robert W. Chambers", "The king in yellow", 316));
+        put("Alma Books", new Book("F Scott Fitzgerald", "The Great Gatsby", 256));
+        put("Orbit", new Book("Andrzej Sapkowski", "The Last Wish: Introducing the Witcher", 384));
+      }
+    };
+
+    myBooksLinkedMap.forEach((key, value) -> {
+      System.out.println("Publishing : " + key + " - " + value.toString());
+    });
+
+    System.out.println("--  Ordem Alfabética.  --");
+
+    Map<String, Book> myBooksTreeMap = new TreeMap<>(myBooksLinkedMap);
+
+    myBooksTreeMap.forEach((key, value) -> {
+      System.out.println("Publishing : " + key + " - " + value.toString());
+    });
+
+    System.out.println("--  Ordem Alfabética (Nome dos Livros).  --");
+
+    Set<Map.Entry<String, Book>> myBooksTreeSet = new TreeSet<>(new BookNameComparator());
+    myBooksTreeSet.addAll(myBooks.entrySet());
+
+    for (Map.Entry<String, Book> book : myBooksTreeSet) {
+      System.out.println(book.getKey() + " - " + book.getValue().getBookName());
+    }
+
+    System.out.println("--  Ordem pelo numero de paginas --");
+
+  }
 }
