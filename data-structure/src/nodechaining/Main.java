a951d12c74f6cdac93fe5d8f16ee81eadf60ea40
Xandowski <xandy-moraes@hotmail.com>
2023-03-14 22:05:31 -0300

    refactoring node class to accept generic type

--- data-structure/src/nodechaining/Main.java
+++ data-structure/src/nodechaining/Main.java
@@ -2,10 +2,10 @@ package nodechaining;
 
 public class Main {
   public static void main(String[] args) {
-    Node node1 = new Node("My node 1");
-    Node node2 = new Node("My node 2");
+    Node<String> node1 = new Node<>("My node 1");
+    Node<String> node2 = new Node<>("My node 2");
     node1.setNextNode(node2);
-    Node node3 = new Node("My node 3");
+    Node<String> node3 = new Node<>("My node 3");
     node2.setNextNode(node3);
 
     System.out.println(node1);
